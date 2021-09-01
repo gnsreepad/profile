ca4575aac5769cae484afa4ae1d77c07f08d68b5
ganeshs1461 <ganesh@keyvalue.systems>
2021-09-01 17:57:56 +0530

    adding portfolio details

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,16 +11,16 @@ export const headData = {
 export const heroData = {
   title: '',
   name: 'Ganesh S',
-  subtitle: 'Software Developer',
+  subtitle: 'Machine Learning Engineer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: 'Content Loading',
-  paragraphTwo: 'Content Loading',
-  paragraphThree: 'Content Loading',
+  paragraphOne: 'Ganesh has expertise in building cloud-based machine learning and natural language processing services at early stage tech companies.',
+  paragraphTwo: 'Built neural networks to analyze and categorize product images, a recommendation system to personalize shopping experiences for each user, and tools to generate insight into user behavior.',
+  paragraphThree: 'Additionaly Ganesh has back end software engineering experience.',
   resume: 'https://github.com/', // if no resume, the button will not show up
 };
 
@@ -28,30 +28,30 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'expression.jpg',
+    title: 'Facial Expression Recognition',
+    info: 'Here we recognize emotions using deep learning model on Kaggle data. The data consists of 48x48 pixel grayscale images of faces. The task is to categorize each face based on the emotion shown in the facial expression in to one of seven categories (0=Angry, 1=Disgust, 2=Fear, 3=Happy, 4=Sad, 5=Surprise, 6=Neutral)',
     info2: '',
     url: '',
-    repo: 'https://github.com/', // if no repo, the button will not show up
+    repo: 'https://github.com/gnsreepad/Facial-Expression-Recognition', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'masters_apply.png',
+    title: 'Masters Apply',
+    info: 'Non-profit organisation to help students prepare for graduate education possibilities in USA',
     info2: '',
-    url: '',
-    repo: 'https://github.com/', // if no repo, the button will not show up
+    url: 'https://mastersapply.com/',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'scissors.jpg',
+    title: 'Rock Paper Scissors',
+    info: 'This program identifies camera image as rock, paper, scissor. Developed a tensorflow model to perform classification, converted the model to Tensorflow Lite to increase performace by 60% in mobile devices',
     info2: '',
     url: '',
-    repo: 'https://github.com/', // if no repo, the button will not show up
+    repo: 'https://github.com/gnsreepad/rock-paper-scissor', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +59,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'gnsreepad@gmail.com',
 };
 
 // FOOTER DATA
@@ -78,12 +78,12 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/gsreepad/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/gnsreepad/',
     },
   ],
 };
